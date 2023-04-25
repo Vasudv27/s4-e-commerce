6c2b626c477bb7121d433dfa731ed51843ddd786
Vasudv <vasudv27@gmail.com>
2023-04-26 03:04:05 +0530

    First video process env file not found

--- conf/mongo.config.js
+++ conf/mongo.config.js
@@ -1,2 +1,11 @@
+const mysql=require("mysql2")
 
+const pool = mysql.createPool({
+    host:process.env.host,
+    user:process.env.user,
+    database:process.env.database,
+    password:process.env.password,
 
+});
+
+module.exports = pool.promise();
